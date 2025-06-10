faff863aa6d6b174642fbf2d78468160ace08e12
Anton Pogrebnjak <contact@pantonius.dev>
2025-06-10 15:46:28 +0200

    Moved harpoon keymaps, added coqlsp

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -78,20 +78,7 @@
         };
 
         # harpoon
-        harpoon = {
-          enable = true;
-
-          # keymaps = {
-          #   addFile = "<leader>a";
-          #   toggleQuickMenu = "<C-e>";
-          #   navFile = {
-          #     "1" = "<A-1>";
-          #     "2" = "<A-2>";
-          #     "3" = "<A-3>";
-          #     "4" = "<A-4>";
-          #   };
-          # };
-        };
+        harpoon.enable = true;
 
         # Language Servers
         lsp = {
@@ -294,9 +281,42 @@
         LanguageTool-nvim
         vimtex
         Coqtail
+        coq-lsp-nvim
       ];
 
       keymaps = [
+        # Harpoon
+        {
+          mode = "n";
+          key = "<leader>a";
+          action.__raw = "function() require'harpoon':list():add() end";
+        }
+        {
+          mode = "n";
+          key = "<C-e>";
+          action.__raw = "function() require'harpoon'.ui:toggle_quick_menu(require'harpoon':list()) end";
+        }
+        {
+          mode = "n";
+          key = "<C-j>";
+          action.__raw = "function() require'harpoon':list():select(1) end";
+        }
+        {
+          mode = "n";
+          key = "<C-k>";
+          action.__raw = "function() require'harpoon':list():select(2) end";
+        }
+        {
+          mode = "n";
+          key = "<C-l>";
+          action.__raw = "function() require'harpoon':list():select(3) end";
+        }
+        {
+          mode = "n";
+          key = "<C-m>";
+          action.__raw = "function() require'harpoon':list():select(4) end";
+        }
+
         # Compile tex file with xelatex
         {
           mode = "n";
